6a7975fde318bb851b9ca5d80e34891bcb4c04dc
phgandhi02 <phgandhi02@gmail.com>
2024-11-16 17:19:32 -0500

    enabled servos on ROS_arduino_bridge

--- ros_arduino_bridge-main/ROSArduinoBridge/ROSArduinoBridge.ino
+++ ros_arduino_bridge-main/ROSArduinoBridge/ROSArduinoBridge.ino
@@ -66,8 +66,8 @@
    #define L298_MOTOR_DRIVER
 #endif
 
-//#define USE_SERVOS  // Enable use of PWM servos as defined in servos.h
-#undef USE_SERVOS     // Disable use of PWM servos
+#define USE_SERVOS  // Enable use of PWM servos as defined in servos.h
+// #undef USE_SERVOS     // Disable use of PWM servos
 
 /* Serial port baud rate */
 #define BAUDRATE     57600
